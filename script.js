5754148f0bbbe0639d3ed61f558781a9044af175
baqarikone <154324770+baqarikone@users.noreply.github.com>
2023-12-19 02:41:25 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Exercise 1: Changing Text Content,Create an HTML page with a <p> element.
+// Write a JavaScript function that, when called, changes the
+// text content of the <p> element to "Hello, DOM!".
+
+// let parag = document.querySelector(".kone");
+// console.log(parag);
+
+// function greeting() {
+//   parag.textContent = "hello dom";
+// }
+// greeting();
+
+//          Exercise 2: Changing Styles
+// Create an HTML page with a <button> element. Write a JavaScript function that,
+// when called, changes the background color of the <button> to
+// red and the text color to white.
+
+// let button = document.querySelector(".btn");
+// function changeColor() {
+//   button.style.color = "white";
+//   button.style.background = "red";
+// }
+// button.addEventListener("click", changeColor);
+
+// Exercise 3: Creating Elements
+// Create an HTML page with an empty <div> element.Write a JavaScript function
+// that, when called, creates a new <p> element with the text content
+// "New paragraph" and appends it as a child to the <div>.
+
+// function create() {
+//   let div = document.createElement("div");
+//   document.body.appendChild(div);
+//   let para = document.createElement("p");
+//   para.textContent = "New paragraph";
+//   div.appendChild(para);
+// }
